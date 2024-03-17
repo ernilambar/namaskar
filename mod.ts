d8c469ddce0ec164033c25597150192304d89348
Nilambar Sharma <nilambar@outlook.com>
2024-03-17 10:48:03 +0545

    Add files

---
+++ mod.ts
@@ -0,0 +1,14 @@
+// mod.ts
+/**
+ * A module providing a function to greet people in Nepali.
+ * @module
+ */
+
+/**
+ * Greet a person in Nepali.
+ *
+ * @param name The name of the person to greet.
+ */
+export function namaskar(name: string) {
+  console.log(`Namaskar, ${name}!`);
+}
